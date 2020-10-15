3d9b4e318ae574eabcaae1013c47a6e1b8d8da66
Wesley <wesley.contato@hotmail.com>
2020-10-15 19:01:04 -0300

    O poder dos sets

---
+++ gerenciador-de-cursos/src/TestaAlunos.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class TestaAlunos {
+
+	public static void main(String[] args) {
+		Collection<String> alunos = new HashSet<>();
+		alunos.add("Rodrigo Turini");
+		alunos.add("Alberto Souza");
+		alunos.add("Nicco Steppat");
+		alunos.add("Sergio Lopes");
+		alunos.add("Renan Saggio");
+		alunos.add("Mauricio Aniche");
+		alunos.add("Alberto Souza");
+
+		boolean pauloEstaMatriculado = alunos.contains("Paulo Silveira");
+		alunos.remove("Sergio Lopes");
+		System.out.println(pauloEstaMatriculado);
+
+		System.out.println(alunos.size());
+
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+
+		alunos.forEach(aluno -> {
+			System.out.println(aluno);
+		});
+
+		System.out.println(alunos);
+
+		List<String> alunosEmLista = new ArrayList<>(alunos);
+
+	}
+
+}
